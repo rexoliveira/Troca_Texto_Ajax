8b629cc4b62dd8f3f2ea3f6089eef03102ba052a
rexoliveira <roolivei@yahoo.com.br>
2022-08-06 00:50:14 -0300

    tarefa aula Ajax OK

--- pagina2.php
+++ pagina2.php
@@ -1,2 +1,5 @@
 <?php
-echo "Essa ideia de verificar o que já tem na lista antes de mandar outra consulta tb é muito boa. Porque se com 4 caracteres já tá trazendo um catatau de resultados, de repente se o 5 seguir os 4 anteriores eu com java-script mesmo já mato os que começarem a fugir do escopo. ainda vou testar o código mas +1 pela ideia." ; 
+
+echo "A função open() irá constituir um canal de comunicação com o
+documento “teste.php”, mas não irá realizar a requisição, para
+isso precisamos adicionar mais um método:" ; 
