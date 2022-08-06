1eec313225dd43d71caa7990ac3e4bf54d13f230
rexoliveira <rex.oliveira15@gmail.com>
2022-08-05 21:58:11 -0300

    Projeto AJAX

---
+++ pagina1.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "Essa ideia de verificar o que já tem na lista antes de mandar outra consulta tb é muito boa. Porque se com 4 caracteres já tá trazendo um catatau de resultados, de repente se o 5 seguir os 4 anteriores eu com java-script mesmo já mato os que começarem a fugir do escopo. ainda vou testar o código mas +1 pela ideia." ; 
